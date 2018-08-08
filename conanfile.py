19c9fb90792ca1f3c5d3b7ddb3782e408a4e0acf
SSE4 <tomskside@gmail.com>
2018-08-08 22:24:22 +0700

    - Windows build
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -31,6 +31,8 @@ class SRTConan(ConanFile):
 
     def requirements(self):
         self.requires.add('OpenSSL/1.0.2o@conan/stable')
+        if self.settings.os == 'Windows':
+            self.requires.add('pthread-win32/2.9.1@bincrafters/stable')
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -47,6 +49,13 @@ class SRTConan(ConanFile):
                                   'set (SSL_LIBRARIES ${OPENSSL_LIBRARIES})',
                                   'set (SSL_LIBRARIES ${OPENSSL_LIBRARIES} dl)')
 
+        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+                              'srt_add_application(srt-multiplex ${VIRTUAL_srtsupport})',
+                              '#srt_add_application(srt-multiplex ${VIRTUAL_srtsupport})')
+        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+                              'srt_add_application(srt-file-transmit ${VIRTUAL_srtsupport})',
+                              '#srt_add_application(srt-file-transmit ${VIRTUAL_srtsupport})')
+
     def configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions['ENABLE_SHARED'] = self.options.shared
@@ -57,6 +66,10 @@ class SRTConan(ConanFile):
         cmake.definitions['OPENSSL_INCLUDE_DIR'] = self.deps_cpp_info['OpenSSL'].include_paths[0]
         cmake.definitions['OPENSSL_LIB_DIR'] = self.deps_cpp_info['OpenSSL'].lib_paths[0]
         cmake.definitions['OPENSSL_LIBRARIES'] = ';'.join(self.deps_cpp_info['OpenSSL'].libs)
+
+        if self.settings.os == 'Windows':
+            cmake.definitions['PTHREAD_INCLUDE_DIR'] = self.deps_cpp_info['pthread-win32'].include_paths[0]
+            cmake.definitions['PTHREAD_LIBRARY'] = self.deps_cpp_info['pthread-win32'].libs[0]
 
         if self.settings.os != 'Windows':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
@@ -73,6 +86,9 @@ class SRTConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['srt']
+        if self.settings.os == 'Windows':
+            self.cpp_info.libs = ['srt' if self.options.shared else 'srt_static']
+        else:
+            self.cpp_info.libs = ['srt']
         if self.settings.os == 'Linux':
             self.cpp_info.libs.append('pthread')
