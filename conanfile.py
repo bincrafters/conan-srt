9a108b53852e4a138d74bc7a4e29044aa87a0651
bincrafters-user <bincrafters@gmail.com>
2020-05-13 04:54:23 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.2

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class SRTConan(ConanFile):
     _build_subfolder = "build_subfolder"
 
     def requirements(self):
-        self.requires.add('openssl/1.1.1f')
+        self.requires.add('openssl/1.1.1g')
         if self.settings.os == 'Windows':
             self.requires.add('pthreads4w/3.0.0')
 
